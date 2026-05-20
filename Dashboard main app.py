a2d8b3ce7e17a8a0c68702fe1018824c736cde7f
FredericoStrasser-UFRJ <strasserfrederico@gmail.com>
2026-05-20 00:25:30 -0300

    Update Dashboard main app.py

--- Dashboard main app.py
+++ Dashboard main app.py
@@ -49,83 +49,174 @@ app.layout = dbc.Container([
     )
 ])
 ])
-
 @callback(
     Output(component_id='graf', component_property='children'),
+    Input(component_id="dropdown-par",component_property="value"),
     Input(component_id='dropdown-art', component_property='value'),
 )
-def update_graphs(input):
+def gera_graficos(input_par, input_art):
 #deixa zerado antes de qualquer input
-    if input is None or input == []:
+    if input_par is None:
         return []
+    if input_art is None or input_art == []:
+        return []
+    if input_par == "c-letras":
 #cria lista vazia para graficos
-    grafs = []
+        grafs = []
 #itera para cada grafico
-    for i in input:
+        for i in input_art:
 #junta todas as palavras usadas pelo artista
-        c = "".join(
-            df[df["corpus_id"] == i]["c_word"].tolist()
-        )
+            c = "".join(
+                df[df["corpus_id"] == i]["c_word"].tolist()
+                )
 #conta o numero de apariçoes de cada letra e gera um dicionario
-        di = {k: Counter(c)[k]
-    for k in ['u', 'p', 'P', 'a', 'A', 's', 'S']
-}
+            di = {k: Counter(c)[k]
+            for k in ['u', 'p', 'P', 'a', 'A', 's', 'S']
+            }
 #calcula porcentagem de uso
-        porcentagem = [
-    (v / sum(di.values())) *100 
-    for v in di.values()
-]
-
+            porcentagem = [
+        (v / sum(di.values())) *100 
+        for v in di.values()
+        ]
 #gera os graficos
-        fig = px.bar(
-            x=list(di.keys()),
-            y=list(porcentagem),
-            labels={
-                "x": "Letras",
-                "y": "Porcentagem de uso"
-            },
-            title=i
-        )
-#centraliza o titulo
-        fig.update_layout(title_x=0.5)
-#da nome pra cada letra
-        labels = {
-    'u': 'Uníssono',
-    'p': 'Passo descendente',
-    'P': 'Passo ascendente',
-    'a': 'Arpejo descendente',
-    'A': 'Arpejo ascendente',
-    's': 'Salto descendente',
-    'S': 'Salto ascendente'
-}
-#gera uma lista com os novos nomes e com os valores de aparição
-        customdata = [
-    [
-        labels[k],
-        v
-    ]
-    for k, v in di.items()
-]
-#define o hover
-        fig.update_traces(
-             customdata=customdata,
-    hovertemplate=
-    "C-letra: %{x}<br>" +
-    "Descrição da C-letra: %{customdata[0]}<br>" +
-    "Percentual de uso: %{y:.2f}%<br>"
-    "Número de usos da letra: %{customdata[1]}<br>"
-    f"Artista: {i}<br>" +
-    "<extra></extra>"
-)
-#adiciona cada grafico em uma coluna 
-        grafs.append(
-            dbc.Col(
-                dcc.Graph(figure=fig),
-                width=4
+            fig = px.bar(
+                x=list(di.keys()),
+                y=list(porcentagem),
+                labels={
+                    "x": "Letras",
+                    "y": "Porcentagem de uso"
+                },
+                title=i
             )
-        )
-# caso a row encha ele automaticamente cria uma nova
-    return dbc.Row(grafs)        
+#centraliza o titulo
+            fig.update_layout(title_x=0.5)
+#da nome pra cada letra
+            labels = {
+        'u': 'Uníssono',
+        'p': 'Passo descendente',
+        'P': 'Passo ascendente',
+        'a': 'Arpejo descendente',
+        'A': 'Arpejo ascendente',
+        's': 'Salto descendente',
+        'S': 'Salto ascendente'
+    }
+    #gera uma lista com os novos nomes e com os valores de aparição
+            customdata = [
+        [
+            labels[k],
+            v
+        ]
+        for k, v in di.items()
+    ]
+    #define o hover
+            fig.update_traces(
+                customdata=customdata,
+        hovertemplate=
+        "C-letra: %{x}<br>" +
+        "Descrição da C-letra: %{customdata[0]}<br>" +
+        "Percentual de uso: %{y:.2f}%<br>"
+        "Número de usos da letra: %{customdata[1]}<br>"
+        f"Artista: {i}<br>" +
+        "<extra></extra>"
+    )
+    #adiciona cada grafico em uma coluna 
+            grafs.append(
+                dbc.Col(
+                    dcc.Graph(figure=fig),
+                    width=4
+
+                )
+            )
+    # caso a row encha ele automaticamente cria uma nova
+        return dbc.Row(grafs)   
+    if input_par == "r-letras":
+#cria lista vazia para graficos
+        grafs = []
+#itera para cada grafico
+        for i in input_art:
+#junta todas as palavras usadas pelo artista
+            c = "".join(
+                df[df["corpus_id"] == i]["r_word"].tolist()
+                )
+#conta o numero de apariçoes de cada letra e gera um dicionario
+            di = {k: Counter(c)[k]
+            for k in ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+            }
+#calcula porcentagem de uso
+            porcentagem = [
+        (v / sum(di.values())) *100 
+        for v in di.values()
+        ]
+#gera os graficos
+            fig = px.bar(
+                x=list(di.keys()),
+                y=list(porcentagem),
+                labels={
+                    "x": "Letras",
+                    "y": "Porcentagem de uso"
+                },
+                title=i
+            )
+#centraliza o titulo
+            fig.update_layout(title_x=0.5)
+#da nome pra cada letra
+            labels = {
+        'a': '□□□□ □□□□ □□□□',
+        'b': '■□□□ □□□□ □□□□',
+        'c': '□□□■ □□□□ □□□□',
+        'd': '□□□□ ■□□□ □□□□',
+        'e': '□□□□ □□■□ □□□□',
+        'f': '□□□□ □□□□ ■□□□',
+        'g': '□□□□ □□□□ □■□□',
+        'h': '■□□■ □□□□ □□□□',
+        'i': '■□□□ ■□□□ □□□□',
+        'j': '■□□□ □□■□ □□□□',
+        'k': '■□□□ □□□□ ■□□□',
+        'l': '■□□□ □□□□ □■□□',
+        'm': '□□□■ □□■□ □□□□',
+        'n': '□□□■ □□□□ □■□□',
+        'o': '□□□□ ■□□□ ■□□□',
+        'p': '□□□□ □□■□ □■□□',
+        'q': '■□□■ □□■□ □□□□',
+        'r': '■□□■ □□□□ □■□□',
+        's': '■□□□ ■□□□ ■□□□',
+        't': '■□□□ □□■□ □■□□',
+        'u': '□□□■ □□■□ □■□□',
+        'v': '■□□■ □□■□ □■□□',
+        'w': '■□□□ □□■□ ■□■□',
+        'x': '■□□■ □□■□ ■□■□',
+        'y': '□□■□ ■□■□ ■□■□',
+        'z': '■□■□ ■□■□ ■□■□'
+    }
+    #gera uma lista com os novos nomes e com os valores de aparição
+            customdata = [
+        [
+            labels[k],
+            v
+        ]
+        for k, v in di.items()
+    ]
+    #define o hover
+            fig.update_traces(
+                customdata=customdata,
+        hovertemplate=
+        "R-letra: %{x}<br>" +
+        "Pontos de ataque: %{customdata[0]}<br>" +
+        "Percentual de uso: %{y:.2f}%<br>"
+        "Número de usos da letra: %{customdata[1]}<br>"
+        f"Artista: {i}<br>" +
+        "<extra></extra>"
+    )
+    #adiciona cada grafico em uma coluna 
+            grafs.append(
+                dbc.Col(
+                    dcc.Graph(figure=fig),
+                    width=12
+
+                )
+            )
+    # caso a row encha ele automaticamente cria uma nova
+        return dbc.Row(grafs)     
 
 if __name__ == '__main__':
     app.run(debug=True)
